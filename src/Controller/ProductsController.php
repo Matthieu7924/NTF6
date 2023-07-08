88e5cdbd641bb0b692eb00b15a84bdd215663bd2
matt <matthieugarban@gmail.com>
2023-07-09 00:30:01 +0200

    adding App\Entity\ 4 services.yaml

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
+
+
+#[Route('/produits', name: 'products_')]
+class ProductsController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('products/index.html.twig');
+    }
+
+    // #[Route('/{slug}', name: 'details')]
+    // public function details(Product $product): Response
+    // {
+    //     dd($product);
+    //     return $this->render('products/details.html.twig');
+    // }
+
+
+    #[Route('/{slug}', name: 'details')]
+    public function details(string $slug, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findOneBy(['slug' => $slug]);
+
+        dd($product);
+        return $this->render('products/details.html.twig');
+    }
+
+
+}
