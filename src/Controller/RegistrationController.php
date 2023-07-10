38bc6f7603c38f699c557bd4f6e9a26ca771e248
matt <matthieugarban@gmail.com>
2023-07-10 11:54:48 +0200

    adding SendMailService & flashMessages

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,21 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Service\SendMailService;
 use App\Form\RegistrationFormType;
 use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager, SendMailService $mail): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -35,6 +36,19 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            
+            //envoi du mail
+            $mail->send(
+                'noreply@monsite.com',
+                $user->getEmail(),
+                'Activation de votre compte sur le site NTS6',
+                'register',
+                compact('user')
+                // [
+                //     'user'=>$user
+                // ]
+                );
+
 
             return $userAuthenticator->authenticateUser(
                 $user,
